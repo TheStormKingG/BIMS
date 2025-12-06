42b898139299326622a0d4d8ae95526519a8b058
TheStormKingG <stefan.gravesande@gmail.com>
2025-12-05 21:00:36 -0400

    Add 404.html for GitHub Pages SPA routing support - enables /overview, /funds, /scan, /spending routes

--- index.tsx
+++ index.tsx
@@ -14,6 +14,21 @@ if ('serviceWorker' in navigator) {
   });
 }
 
+// Handle GitHub Pages 404 redirect
+// If we're on GitHub Pages and the URL has a query parameter starting with /?/
+// (which means it was redirected from 404.html), we need to restore the original path
+if (window.location.search.includes('?/')) {
+  const path = window.location.search
+    .replace(/^\?\/?/, '')
+    .replace(/~and~/g, '&')
+    .split('&')[0];
+  
+  if (path) {
+    const newPath = '/BIMS/' + path + window.location.hash;
+    window.history.replaceState({}, '', newPath);
+  }
+}
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
